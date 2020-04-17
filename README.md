2a5fb7582a7441a87eefc4bc6d84c2ffda3b283b
Zack Davenport <zackdavenport@tcadmins-Air.broadband>
2020-04-17 12:20:42 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# cookbook-environment
+
+TODO: Enter the cookbook description here.
+
---
+++ spec/unit/recipes/default_spec.rb
@@ -0,0 +1,30 @@
+#
+# Cookbook:: cookbook-environment
+# Spec:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+
+require 'spec_helper'
+
+describe 'cookbook-environment::default' do
+  context 'When all attributes are default, on Ubuntu 18.04' do
+    # for a complete list of available platforms and versions see:
+    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
+    platform 'ubuntu', '18.04'
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+    it 'should apt get update' do
+      expect(chef_run).to update_apt_update 'update_sources'
+    end
+
+    it 'should install python3-pip' do
+      expect(chef_run).to install_package 'python3-pip'
+    end
+
+    it 'should create a Downloads folder' do
+      expect(chef_run).to create_directory '/home/vagrant/Downloads'
+    end
+  end
+end
